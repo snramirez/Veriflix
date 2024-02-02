591d1d2e65a024adf11e206de0a8a57cb4712fed
snramirez <54188400+snramirez@users.noreply.github.com>
2024-02-02 02:20:00 -0300

    project born
    Project creation and a not complete login page.

---
+++ Veriflix/src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from "pinia";
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+const pinia = createPinia();
+const vuetify = createVuetify({
+    components,
+    directives,
+    })
+
+app.use(router)
+app.use(pinia)
+app.use(vuetify)
+app.mount('#app')
---
+++ Veriflix/src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="py-10">
+    <v-card class="mx-auto my-12" max-width="450">
+      <h1 class="d-flex justify-center pt-8">Inicio de Sesion</h1>
+
+      <v-container class="pa-7">
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          @submit.prevent="submitForm"
+        >
+          <v-text-field
+            v-model="user"
+            :rules="userRules"
+            label="Usuario"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            type="password"
+            v-model="password" 
+            :rules="passwordRules"           
+            label="Contraseña"
+            required
+          ></v-text-field>
+
+          <div class="d-flex justify-center py-5">
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              type="submit"
+            >
+              Ingresar
+            </v-btn>
+          </div>
+        </v-form>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const user = ref('')
+const password = ref('')
+const valid = ref(true)
+const userRules = [(v) => !!v || "El usuario es obligatorio"]
+const passwordRules = [(v) => !!v || "La contraseña es obligatoria"]
+
+const submitForm = () => {
+    console.log("send")
+}
+
+</script>
+
+<style></style>
